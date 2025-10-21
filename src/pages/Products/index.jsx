b20872a74f7a5f0d5ad7e5924ab16f43a5abdbe4
Henrique Versiani <henriversiani@gmail.com>
2025-10-21 18:58:21 -0300

    tela cadastro produto update

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -5,18 +5,16 @@ import Header from '../../Components/Header'
 import { Link, useNavigate } from 'react-router'
 import axios from 'axios'
 
-
-
 export default function Products() {
 
     const [products, setProducts] = useState([])
-    const [nome, setNome] = useState([])
-    const [preco, setPreco] = useState([])
-    const [categoria, setCategoria] = useState([])
-    const [imagem, setImagem] = useState([])
-    const [status, setStatus] = useState([])
-    const [genero, setGenero] = useState([])
-    const [classificacao, setClassificacao] = useState([])
+    const [nome, setNome] = useState("")
+    const [preco, setPreco] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [imagem, setImagem] = useState(null)
+    const [status, setStatus] = useState("Disponivel")
+    const [genero, setGenero] = useState("")
+    const [classificacao, setClassificacao] = useState("")
     const [showForm, setShowForm] = useState(false)
 
     const navigate = useNavigate()
@@ -27,31 +25,51 @@ export default function Products() {
     }
 
     async function createProduct(e) {
-
         e.preventDefault()
 
+        let imageUrl = ""
+
+        if (imagem) {
+            const formData = new FormData()
+            formData.append("image", imagem)
+
+            const imgbbApiKey = "d345d40546abe58bf622c91ca3c57470"
+            const uploadResponse = await axios.post(
+                `https://api.imgbb.com/1/upload?key=${imgbbApiKey}`,
+                formData
+            )
+
+            imageUrl = uploadResponse.data.data.display_url
+        }
+
         const req = await axios.post('http://localhost:3000/products', {
-            "nome": nome,
-            "preco": Number(preco),
-            "categoria": categoria,
-            "imagem": imagem,
-            "status": status,
-            "genero": genero,
-            "classificacao": classificacao,
+            nome,
+            preco: Number(preco),
+            categoria,
+            imagem: imageUrl,
+            status,
+            genero,
+            classificacao,
         })
+
+        const res = await req.data
+
+        if (res.error) {
+            toast.warning(res.error)
+        } else {
+            toast.success("User Created")
+            setTimeout(() => {
+                navigate("/deposit")
+            }, 2000)
+        }
     }
-
-
 
     useEffect(() => {
         getProducts()
-        const tokenLocal = localStorage.getItem('token');
-        const emailLocal = localStorage.getItem('email');
-
+        const tokenLocal = localStorage.getItem('token')
         if (!tokenLocal) {
             navigate("/")
         }
-
     }, [])
 
     return (
@@ -59,72 +77,43 @@ export default function Products() {
             <Header />
             <h1> Products </h1>
 
-
             {showForm ? (
                 <div className='container'>
-                    <Link className='link-form-back' onClick={() => setShowForm(false)}> ←  Back</Link>
-                    <form className='form-product'>
+                    <button className='link-form-back' onClick={() => setShowForm(false)}> ← Back</button>
+                    <form className='form-product' onSubmit={createProduct}>
                         <h1>Add Product</h1>
-                        <input type="text" placeholder='Name' value={nome} onChange={({ target }) => { setNome(target.value); }} />
-                        <input type="text" placeholder='Price' value={preco} onChange={({ target }) => { setPreco(target.value); }} />
-                        <input type="text" placeholder='Category' value={categoria} onChange={({ target }) => { setCategoria(target.value); }} />
-                        <input type="text" placeholder='Image' value={imagem} onChange={({ target }) => { setImagem(target.value); }} />
-                        <input type="text" placeholder='Status' value={status} onChange={({ target }) => { setStatus(target.value); }} />
-                        <input type="text" placeholder='Gender' value={genero} onChange={({ target }) => { setGenero(target.value); }} />
-                        <input type="text" placeholder='Class' value={classificacao} onChange={({ target }) => { setClassificacao(target.value); }} />
+                        <input type="text" placeholder='Name' value={nome} onChange={({ target }) => setNome(target.value)} />
+                        <input type="text" placeholder='Price' value={preco} onChange={({ target }) => setPreco(target.value)} />
+                        <input type="text" placeholder='Category' value={categoria} onChange={({ target }) => setCategoria(target.value)} />
 
-                        <button className='button-signup' onClick={createProduct}>Add</button>
+                        <input type="file" onChange={({ target }) => setImagem(target.files[0])} />
+
+                        <input type="text" placeholder='Gender' value={genero} onChange={({ target }) => setGenero(target.value)} />
+                        <input type="text" placeholder='Class' value={classificacao} onChange={({ target }) => setClassificacao(target.value)} />
+
+                        <button className='button-signup' type='submit'>Add</button>
                     </form>
                 </div>
             ) : (
                 <div>
-                <div className='main'>
-                    <Link className='link-form' onClick={() => setShowForm(true)}>Add Product</Link>
-                </div>
-                {products.map(product => (
-                <div key={product.id} className='container'>
-                    <div className='card'>
-
-                        <div className='info'>
-                            <h3>Name:</h3>
-                            <p className='nome'>{product.nome}</p>
-                        </div>
-
-                        <div className='info'>
-                            <h3>Categoria:</h3>
-                            <p className='email'>{product.categoria}</p>
-                        </div>
-
-                        <div className='info'>
-                            <h3>Gênero:</h3>
-                            <p className='area-ocupacao'>{product.genero}</p>
-                        </div>
-
-                        <div className='info'>
-                            <h3>Preço:</h3>
-                            <p className='area-ocupacao'>{product.preco}</p>
-                        </div>
-
-                        <div className='info'>
-                            <h3>Status:</h3>
-                            <p className='area-ocupacao'>{product.status}</p>
-                        </div>
-
-                        <div className='info'>
-                            <h3>Id:</h3>
-                            <p className='area-ocupacao'>{product._id}</p>
-                        </div>
-
-                        <div className='info'>
-                            <h3>Link Imagem:</h3>
-                            <p className='area-ocupacao'>{product.imagem}</p>
-                        </div>
-
+                    <div className='main'>
+                        <button className='link-form' onClick={() => setShowForm(true)}>Add Product</button>
                     </div>
+                    {products.map(product => (
+                        <div key={product.id} className='container'>
+                            <div className='card'>
+                                <div className='info'><h3>Name:</h3><p>{product.nome}</p></div>
+                                <div className='info'><h3>Categoria:</h3><p>{product.categoria}</p></div>
+                                <div className='info'><h3>Gênero:</h3><p>{product.genero}</p></div>
+                                <div className='info'><h3>Preço:</h3><p>{product.preco}</p></div>
+                                <div className='info'><h3>Status:</h3><p>{product.status}</p></div>
+                                <div className='info'><h3>Id:</h3><p>{product._id}</p></div>
+                                <img src={product.imagem} alt="Product Image" />
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
-            </div>
-            )}  
+            )}
         </>
     )
 }
