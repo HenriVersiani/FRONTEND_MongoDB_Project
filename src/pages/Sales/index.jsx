0cce547758602cf945fff5d7357074f76983557f
Henrique Versiani <henriversiani@gmail.com>
2025-10-30 18:48:15 -0300

    css

--- src/pages/Sales/index.jsx
+++ src/pages/Sales/index.jsx
@@ -74,7 +74,7 @@ export default function Sales() {
         <>
             <Header />
             <h1> Sales </h1>
-            <ToastContainer/>
+            <ToastContainer />
             {showForm ?
                 (
                     <div className='container'>
@@ -114,30 +114,35 @@ export default function Sales() {
                         {sales.map(sale => (
                             <div key={sale.id} className='container'>
                                 <div className='card'>
+                                    <div className='card-info'>
+                                        <div className='info'>
+                                            <h3>Id:</h3>
+                                            <p className='area-ocupacao'>{sale._id}</p>
+                                        </div>
 
-                                    <div className='info'>
-                                        <h3>Id:</h3>
-                                        <p className='area-ocupacao'>{sale._id}</p>
+                                        <div className='info'>
+                                            <h3>Product Id:</h3>
+                                            <p className='nome'>{sale.idProduto}</p>
+                                        </div>
+
+                                        <div className='info'>
+                                            <h3>Seller Id:</h3>
+                                            <p className='email'>{sale.idVendedor}</p>
+                                        </div>
+
+                                        <div className='info'>
+                                            <h3>Payment Method:</h3>
+                                            <p className='area-ocupacao'>{sale.metodoPagamento}</p>
+                                        </div>
+
+                                        <div className='info'>
+                                            <h3>Sell Price:</h3>
+                                            <p className='area-ocupacao'>{sale.precoVenda}</p>
+                                        </div>
                                     </div>
-
-                                    <div className='info'>
-                                        <h3>Product Id:</h3>
-                                        <p className='nome'>{sale.idProduto}</p>
-                                    </div>
-
-                                    <div className='info'>
-                                        <h3>Seller Id:</h3>
-                                        <p className='email'>{sale.idVendedor}</p>
-                                    </div>
-
-                                    <div className='info'>
-                                        <h3>Payment Method:</h3>
-                                        <p className='area-ocupacao'>{sale.metodoPagamento}</p>
-                                    </div>
-
-                                    <div className='info'>
-                                        <h3>Sell Price:</h3>
-                                        <p className='area-ocupacao'>{sale.precoVenda}</p>
+                                    <div className='buttons'>
+                                        <button className='button-delete' onClick={() => deleteUser(user._id)}>Delete</button>
+                                        <button className='button-edit' onClick={() => navigate(`/seller/${user._id}`)}>Edit</button>
                                     </div>
                                 </div>
                             </div>
